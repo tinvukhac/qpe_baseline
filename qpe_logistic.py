23674860d3c9a5470114215142cc46d3d09c11cb
tinvukhac <tinvukhac@gmail.com>
2018-02-27 14:26:11 -0800

    update logistic regression function

--- qpe_logistic.py
+++ qpe_logistic.py
@@ -13,6 +13,7 @@ import numpy as np
 from sklearn import linear_model
 
 
+# Load the train and test data
 reader = csv.reader(open("train.csv", "rb"), delimiter=",")
 x = list(reader)
 train_data = np.array(x).astype("float")
@@ -20,17 +21,24 @@ reader = csv.reader(open("test.csv", "rb"), delimiter=",")
 x = list(reader)
 test_data = np.array(x).astype("float")
 
+# Divide data into features and target
 X_train = train_data[:, :-1]
 Y_train = train_data[:, train_data.shape[1] - 1]
-h = .02  # step size in the mesh
-print X_train.shape
-logreg = linear_model.LogisticRegression(C=1e5)
-
-# we create an instance of Neighbours Classifier and fit the data.
-logreg.fit(X_train, Y_train)
-
 X_test = test_data[:, :-1]
 Y_test = test_data[:, test_data.shape[1] - 1]
+
+# Remove columns with all zeros
+X = np.concatenate((X_train, X_test), axis=0)
+X = X[:, ~np.all(X == 0, axis=0)]
+X_train = X[0:len(X_train), :]
+X_test = X[len(X_train):len(X), :]
+
+# Create an instance of Neighbours Classifier and fit the data.
+h = .02
+logreg = linear_model.LogisticRegression(C=1e5)
+logreg.fit(X_train, Y_train)
+
+# Predict with test data
 Z = logreg.predict(X_test)
 correct = 0
 for i in range(0, len(Z)):
